fcf26b4b01821e836bb576e681c472b3049c022d
Anchit-Nayak <anchitnayak2003@gmail.com>
2024-02-02 21:29:05 +0530

    database and routers added

---
+++ server/Controllers/validateForm.js
@@ -0,0 +1,21 @@
+const Yup = require("yup");
+
+const formSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+})
+
+const validateForm = (req, res, next) => {
+    const formData = req.body;
+    formSchema.validate(formData)
+        .then((valid) => {
+            if(valid){
+                console.log("form valid");
+            }
+        })
+        .catch((err) => {
+            res.status(400).json(err.errors)
+        })
+}
+
+module.exports = validateForm;
