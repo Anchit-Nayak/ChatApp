7d06ce0a36ba9d25dd30fa68950544686386848a
Anchit Nayak <113703908+Anchit-Nayak@users.noreply.github.com>
2024-02-11 00:26:49 +0530

    Delete common directory

--- common/index.js
+++
@@ -1,8 +0,0 @@
-const Yup = require('yup');
-
-const formSchema= Yup.object({
-    username: Yup.string().required("Username is required"),
-    password: Yup.string().required("Password is required").min(5, "Password is too short")
-})
-
-module.exports = { formSchema };
